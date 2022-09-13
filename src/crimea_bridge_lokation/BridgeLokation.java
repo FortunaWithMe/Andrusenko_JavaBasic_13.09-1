675bcfdbe0e4f7778a7bc5920df32b6d8217a4bf
Andrusenko Dmytro <Dima2mobile@gmail.com>
2022-09-13 15:29:47 +0300

    first commit

---
+++ src/crimea_bridge_lokation/BridgeLokation.java
@@ -0,0 +1,22 @@
+package crimea_bridge_lokation;
+
+public class BridgeLokation {
+    public static void main(String[] args) {
+        double a = 45.3086;
+        double b = 36.5061;
+
+        System.out.println(a);
+        System.out.println(b);
+
+        String a1 = "45° 18′ 30.96″ N";
+        String b2 = "36° 30′ 21.96″ E";
+
+        System.out.println(a1);
+        System.out.println(b2);
+
+        /*
+        Не було вкзано в якій самі системі координт потрібно вивести результат.
+        Логічно що так як першому варіанті, але на всякий випадок зробив обидва.
+        */
+    }
+}
